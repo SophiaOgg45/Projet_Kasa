a5386780be9e2c8770ef63bb5b54d9a19898fb15
Your Name <sophia.ogg45@hotmail.com>
2024-04-25 14:23:58 +0200

    installation du projet

---
+++ src/compoments/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+    return (
+    <div className="header">
+        <img src ="" alt = "logo kasa" />
+        <nav>
+            <ul>
+                <li>Accueil</li>
+                <li>A propos</li>
+            </ul>
+        </nav>
+    </div>
+    );
+  };
+  
+  export default Header; 
