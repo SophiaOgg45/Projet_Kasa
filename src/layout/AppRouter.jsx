52801204111dc4539de8ffc3cecb778a1e614b8c
Your Name <sophia.ogg45@hotmail.com>
2024-04-29 11:30:12 +0200

    création de la page Error

--- src/layout/AppRouter.jsx
+++ src/layout/AppRouter.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Home from "../pages/Home";
@@ -6,18 +6,17 @@ import About from "../pages/About";
 import Logement from "../pages/Logement";
 import Error from "../pages/Error";
 
-
-
-
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
+        
           <Route path="/" element={<Home />} />
           <Route path="/About" element={<About />} />
           <Route path="/Logement" element={<Logement />} /> 
-          <Route path="/Error" element={<Error />} />         
+          <Route path="*" element={< Error />} />
+                
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,14 @@
+import { NavLink } from 'react-router-dom'
+import nbre from '../assets/images/404.png'; // Importez l'image
 
-const   Error = () => {
+const Error = () => {
     return (
-        <div>
-            <p>Error</p>
-            
+        <div className="error">
+            <img src={nbre} alt="le nombre 404 qui indique la page en erreur" /> {/* Utilisez src au lieu de image */}
+            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <NavLink to="/">
+                Retourner sur la page d’accueil
+            </NavLink>
         </div>
     );
 };
